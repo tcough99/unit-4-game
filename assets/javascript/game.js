ceaddb671e180a369819b9dad1a01aa39d3ae2e8
Thomas Coughlin <tcoughlin@elon.edu>
2019-09-07 08:58:30 -0400

    Finished Version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,8 +15,9 @@ $( document ).ready(function(){
   $('#numberLosses').text(losses);
 
   function reset(){
-        Random=Math.floor(Math.random()*101+19);
-        console.log(Random)
+
+
+    Random=Math.floor(Math.random()*101+19);
         $('#randomNumber').text(Random);
         num1= Math.floor(Math.random()*11+1);
         num2= Math.floor(Math.random()*11+1);
@@ -25,60 +26,59 @@ $( document ).ready(function(){
         userTotal= 0;
         $('#finalTotal').text(userTotal);
         } 
-  function yay(){
+             function winner(){
   alert("You won!");
     wins++; 
     $('#numberWins').text(wins);
     reset();
   }
-  function loser(){
+            function loser(){
   alert ("You lose!");
     losses++;
     $('#numberLosses').text(losses);
     reset()
   }
+
     $('#one').on ('click', function(){
       userTotal = userTotal + num1;
-      console.log("New userTotal= " + userTotal);
       $('#finalTotal').text(userTotal); 
           if (userTotal == Random){
-            yay();
+            winner();
           }
           else if ( userTotal > Random){
             loser();
           }   
     })  
+
     $('#two').on ('click', function(){
       userTotal = userTotal + num2;
-      console.log("New userTotal= " + userTotal);
       $('#finalTotal').text(userTotal); 
           if (userTotal == Random){
-            yay();
+            winner();
           }
-          else if ( userTotal > Random){
+             else if ( userTotal > Random){
             loser();
           } 
     })  
+
     $('#three').on ('click', function(){
       userTotal = userTotal + num3;
-      console.log("New userTotal= " + userTotal);
       $('#finalTotal').text(userTotal);
             if (userTotal == Random){
-            yay();
+            winner();
           }
-          else if ( userTotal > Random){
+            else if ( userTotal > Random){
             loser();
           } 
     })  
+
     $('#four').on ('click', function(){
       userTotal = userTotal + num4;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
-        
+      $('#finalTotal').text(userTotal);     
             if (userTotal == Random){
-            yay();
+            winner();
           }
-          else if ( userTotal > Random){
+             else if ( userTotal > Random){
             loser();
           }
     });   
